461f8690d006b862aa15f25d11ca5c16e7c21e4d
hager283 <hager283@umn.edu>
2022-11-27 16:17:20 -0600

    finished

--- keyword_collector.py
+++ keyword_collector.py
@@ -7,38 +7,42 @@ from nltk.corpus import stopwords
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 
-arr1 = ["special","purpose", "entity", "chewco", "whitewing", "ljm", "ljm1", "ljm2", "raptor", "transfer", "offshore", "losses", "accounting", "fix", "adjustment", "conceal", "hide", "cover", "up", "trouble","shit", "fuck", "destroy", "upset", "angry", "idiot", "wtf", "fire"]
 
-path = 'C:/Users/Mohammed Hager/Documents/Python_Analytics/dataset'
 
-email_id = []
-unique_sent_emails = []
+keyword_emails = []
+
 
 def dedup():
-    text = []
+    path = 'C:/Users/Mohammed Hager/Documents/Python_Analytics/dataset'
+    email_contents  = []
+    unique_sent_emails = []
     for filename in glob.glob(os.path.join(path, '*.json')):    
         with open(filename, encoding='utf-8', mode='r') as currentFile:
             data=currentFile.read().replace('\n', '')
             json_data = json.loads(data)
-            date = json_data["text"]
-            if (date not in text) and (len(date) <= 5000):
+            date = json_data["text"].lower()
+            if (date not in email_contents) and (len(date) <= 5000):
                 unique_sent_emails.append(filename)
             else:
-                text.append(date)
+                email_contents.append(date)
+    return unique_sent_emails
+
+keyword_list = ["special","purpose", "entity", "chewco", "whitewing", "ljm", "ljm1", "ljm2", "raptor", "transfer", "offshore", "losses", "accounting", "fix", "adjustment", "conceal", "hide", "cover", "up", "trouble","shit", "fuck", "destroy", "upset", "angry", "idiot", "wtf", "fire"]
 
 def keywordList(data, emailID, From):
     if "@enron.com" in From:
         data = [x.lower() for x in data]
-        found = set(arr1).intersection(set(data))
+        found = set(keyword_list).intersection(set(data))
         master = str(emailID)+","+str(From)
         if found:
             master += ","+ str(len(found))
             for x in found:
                 master += "," + str(x)
             master.replace("\\","\\\\")
-            email_id.append(master)
+            keyword_emails.append(master)
 
-def main():   
+def main(unique_sent_emails):   
+    path = 'C:/Users/Mohammed Hager/Documents/Python_Analytics/dataset'
     for filename in glob.glob(os.path.join(path, '*.json')):    
         if filename in unique_sent_emails:
             with open(filename, encoding='utf-8', mode='r') as currentFile:
@@ -50,10 +54,10 @@ def main():
                 keywordList(frequency.keys(), filename, json_data["headers"]["from"])
                 
     f = open("keyword_combine.csv", "w+")
-    for x in email_id:
+    for x in keyword_emails:
         f.write(x+"\n")
     f.close()
 
 if __name__ == "__main__":
-    dedup()
-    main()
+    arr = dedup()
+    main(arr)
